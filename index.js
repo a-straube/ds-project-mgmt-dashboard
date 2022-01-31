f4350a42445d6918fbf130a9344a3cc46a7c5976
Axel <axstraube@gmail.com>
2022-01-31 10:28:16 -0800

    Production phase chart styling/naming updates

--- index.js
+++ index.js
@@ -93,38 +93,17 @@ fillProductionPhaseDataList = () => {
 
 makePhaseChart = () => {
     fillProductionPhaseDataList();
-    const ctx = document.getElementById('phaseChart').getContext('2d');
+    const ctx = document.getElementById('productionPhaseChart').getContext('2d');
     const myChart = new Chart(ctx, {
         type: 'line',
         data: {
             labels: ['To Do', 'Phase 1', 'Phase 2', 'Phase 3', 'QA', 'Complete'],
             datasets: [{
                 data: [toDoCount, phaseOneCount, phaseTwoCount, phaseThreeCount, qaCount, completeCount],
-                backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(54, 162, 235, 0.2)',
-                    'rgba(255, 206, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)',
-                    'rgba(153, 102, 255, 0.2)',
-                    'rgba(255, 159, 64, 0.2)'
-                ],
-                borderColor: [
-                    'rgba(255, 99, 132, 1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(255, 159, 64, 1)'
-                ],
+                backgroundColor: '#000000',
+                borderColor: '#000000',
                 borderWidth: 1
             }]
-        },
-        options: {
-            scales: {
-                y: {
-                    beginAtZero: true
-                }
-            }
         }
     });
 }
