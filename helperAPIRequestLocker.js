0197771add70378189cafcb9d00d2a1d476c8a48
Axel <axstraube@gmail.com>
2022-12-02 19:29:18 -0800

    Fix get members request

--- helperAPIRequestLocker.js
+++ helperAPIRequestLocker.js
@@ -1,5 +1,5 @@
 // GET MEMBERS
-// Trello.get('boards/QCJDklm5/memberships', function(members) {
+// Trello.get('boards/QCJDklm5/members', function(members) {
 //     $.each(members, function(ix, member) {
 //         console.log(member);
 //     })
