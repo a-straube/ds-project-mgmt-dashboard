e6da3d70dedf6824669da666f3e8064479b24641
Axel <axstraube@gmail.com>
2022-12-02 20:32:57 -0800

    Update dummy Trello data

--- helperAPIRequestLocker.js
+++ helperAPIRequestLocker.js
@@ -1,8 +1,8 @@
 // GET MEMBERS
-// Trello.get('boards/QCJDklm5/members', function(members) {
-//     $.each(members, function(ix, member) {
-//         console.log(member);
-//     })
-// }, function (error){
-//     console.log(error);
-// });
+Trello.get('boards/QCJDklm5/members', function(members) {
+    $.each(members, function(ix, member) {
+        console.log(member);
+    })
+}, function (error){
+    console.log(error);
+});
