206bb03db59e47e04274f6f60ef852c00ebc8eda
Axel <axstraube@gmail.com>
2022-12-02 19:25:33 -0800

    Add helper API request functions locker

---
+++ helperAPIRequestLocker.js
@@ -0,0 +1,8 @@
+// GET MEMBERS
+// Trello.get('boards/QCJDklm5/memberships', function(members) {
+//     $.each(members, function(ix, member) {
+//         console.log(member);
+//     })
+// }, function (error){
+//     console.log(error);
+// });
